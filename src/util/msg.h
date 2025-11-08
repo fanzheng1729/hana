e6abafeb8cb43144cfe7304333e683fd48f46cbb
fanzheng1729 <58038971+fanzheng1729@users.noreply.github.com>
2025-11-08 20:45:34 +0100

    msg.h

--- src/util/msg.h
+++ src/util/msg.h
@@ -2,7 +2,6 @@
 #define MSG_H_INCLUDED
 
 const char extraendscope[] = "$} without corresponding ${";
-const char itemonstack[] = " items on stack";
 const char varfound[] = " variables found, ";
 const char varallowed[] = " variables allowed";
 const char stackszerr[] = " does not end with only one item on the stack";
