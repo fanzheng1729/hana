6a852d2fef04973511645e97488427c89113c9fe
fanzheng1729 <58038971+fanzheng1729@users.noreply.github.com>
2025-11-12 19:55:22 +0100

    environ.cpp

--- src/search/environ.cpp
+++ src/search/environ.cpp
@@ -139,8 +139,8 @@ Moves Environ::ourmoves(Game const & game, stage_t stage) const
 Eval Environ::evalourleaf(Game const & game) const
 {
     if (game.ndefer == 0 && !game.goaldata().hypstotrim.empty())
-    if (Environ * pnewenv
-        = pProb->addenv(game.penv, game.goaldata().hypstotrim))
+    if (Environ * const pnewenv =
+        pProb->addenv(game.penv, game.goaldata().hypstotrim))
         const_cast<Game &>(game).penv = pnewenv;
     return score(game.env().hypslen + game.goal().size() + game.ndefer);
 }
