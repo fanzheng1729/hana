63050bdbafa30c4fae1088de915941b0bf076ce9
fanzheng1729 <58038971+fanzheng1729@users.noreply.github.com>
2025-11-25 20:01:55 +0100

    loop.cpp

--- src/search/loop.cpp
+++ src/search/loop.cpp
@@ -3,19 +3,6 @@
 // If goal appears as the goal of a node or its ancestors,
 // return the pointer of the ancestor.
 // This check is necessary to prevent self-assignment in writeproof().
-static Nodeptr cycles(Goalptr pGoal, Nodeptr pNode)
-{
-    while (true)
-    {
-        Game const & game = pNode->game();
-        if (game.nDefer == 0 && pGoal == game.pGoal)
-            return pNode;
-        if (Nodeptr const parent = pNode.parent())
-            pNode = parent.parent();
-        else break;
-    }
-    return Nodeptr();
-}
 static Nodeptr cycles(Goaldataptr pGoaldata, Nodeptr pNode)
 {
     while (true)
@@ -62,6 +49,37 @@ struct Goalptrs : std::set<Goalptr>
                     return newGoal;
             }
         return Goalptr();
+    }
+};
+struct Goaldataptrs : std::set<Goaldataptr>
+{
+    // Return true if all open children of p are present.
+    bool haschildren(Nodeptr p) const
+    {
+        FOR (Nodeptr const child, *p.children())
+        {
+            if (child->game().proven())
+                continue;
+            if (!count(child->game().pGoaldata))
+                return false;
+        }
+        return true;
+    }
+    // Return pointer to a new goal implied by the existing goals.
+    // Return NULL if there is no such goal.
+    Goaldataptr saturate()
+    {
+        FOR (Goaldataptr const pGoal, *this)
+            FOR (Nodeptr const pNode, pGoal->second.nodeptrs)
+            {
+                Nodeptr const parent = pNode.parent();
+                if (parent->game().proven())
+                    continue;
+                Goaldataptr const newGoal = parent->game().pGoaldata;
+                if (haschildren(parent) && insert(newGoal).second)
+                    return newGoal;
+            }
+        return Goaldataptr();
     }
 };
 }
