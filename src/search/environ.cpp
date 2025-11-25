8719999dfc460f9fdf3ace8145905e8ae82e4802
fanzheng1729 <58038971+fanzheng1729@users.noreply.github.com>
2025-11-25 17:30:05 +0100

    environ.cpp

--- src/search/environ.cpp
+++ src/search/environ.cpp
@@ -19,7 +19,6 @@ bool proven(Goalptr pGoal, Goaldataptr pGoaldata, Assertion const & ass)
     Hypsize const i = ass.matchhyp(pGoal->first.RPN, pGoal->first.typecode);
     if (i == ass.hypcount()) return false; // No match
     // 1-step proof using the matched hypothesis
-    pGoal->second.proof.assign(1, ass.hypiters[i]);
     pGoaldata->second.proof.assign(1, ass.hypiters[i]);
     return pGoal->second.status = pGoaldata->second.status = GOALTRUE;
 }
