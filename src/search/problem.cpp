143d72715a3dfc8861d64d548d9f3d05ffb63282
fanzheng1729 <58038971+fanzheng1729@users.noreply.github.com>
2025-11-30 03:30:36 +0100

    problem.cpp

--- src/search/problem.cpp
+++ src/search/problem.cpp
@@ -41,7 +41,8 @@ Eval Problem::evalleaf(pNode p) const
     if (game.proven())
         return EvalWIN;
     if (ranksimplerthanProb(game))
-        (std::cout << game.env().name << std::endl << game.goal().expression()),
+        std::cout << assertion.number << game.env().name << std::endl,
+        std::cout << game.goal().expression(),
         std::cin.get();
     return game.env().evalourleaf(game);
 }
