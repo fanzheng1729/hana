4b23bfbb22ced6217ab47dc903290758fbe473ae
fanzheng1729 <58038971+fanzheng1729@users.noreply.github.com>
2025-12-08 11:52:37 +0100

    environ.cpp

--- src/search/environ.cpp
+++ src/search/environ.cpp
@@ -315,8 +315,7 @@ Environ::MoveValidity Environ::validconjmove(Move const & move) const
     // Add the essential hypothesis as a goal.
     pGoal const pgoal = pProb->addgoal(move.absconjs.back(), *penv, GOALNEW);
 std::cout << "Validating " << pgoal->second.goal().expression();
-    Goalstatus const s = penv->status(pgoal->second.goal());
-    std::cout << s; throw;
+    std::cout << penv->status(pgoal->second.goal()); throw;
     return MoveINVALID;
 }
 
