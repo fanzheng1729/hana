ff057d1a881b7044909a40aa5a1f4cad672cbab4
fanzheng1729 <58038971+fanzheng1729@users.noreply.github.com>
2025-12-29 10:24:56 +0100

    typecode

--- src/typecode.h
+++ src/typecode.h
@@ -11,28 +11,25 @@ struct Typecodes : std::map<std::string, std::pair<std::string, bool> >
 {
     Typecodes() {}
     Typecodes(struct Commands const & syntax, Commands const & bound);
-    // Return 1 if a type code is primitive. Return -1 if not found.
     Tribool isprimitive(strview typecode) const
     {
         const_iterator const iter(find(typecode));
         if (iter == end()) return UNKNOWN;
         return iter->second.first.empty() ? TRUE : FALSE;
     }
-    // Return 1 if a type code can be bound. Return -1 if not found.
     Tribool isbound(strview typecode) const
     {
         const_iterator const iter(find(typecode));
         if (iter == end()) return UNKNOWN;
         return iter->second.second ? TRUE : FALSE;
     }
-    // Return the normalized type code. Return "" if not found.
     strview normalize(strview typecode) const
     {
-        const_iterator iter(find(typecode));
-        if (iter == end()) return "";
-        while (!iter->second.first.empty())
-            iter = find(iter->second.first);
-        return iter->first;
+        for (const_iterator iter = find(typecode);
+             iter != end() && !iter->second.first.empty();
+             iter = find(typecode))
+            typecode = iter->second.first;
+        return typecode;
     }
 };
 
