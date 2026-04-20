06c1f5fe750f641f66f246a0965f7ee7d66d298d
fanzheng1729 <58038971+fanzheng1729@users.noreply.github.com>
2026-04-20 17:13:08 +0200

    problem test

--- src/search/problem.cpp
+++ src/search/problem.cpp
@@ -149,33 +149,3 @@ Environ const * Problem::addsupEnv(Environ const & env, Move const & move)
     // Pointer to the super-context
     return newEnviter->second = initEnv(env.makeEnv(supAss));
 }
-
-// Test proof search. Return tree.size if okay. Return 0 if not.
-Treesize testsearch(Assiter iter, Problem & tree, Treesize maxsize)
-{
-    // printass(*iter);
-    tree.play(maxsize);
-    // tree.printstats();
-    // std::cin.get();
-    // if (iter->first == "impbii")
-    //     tree.navigate();
-    // if (iter->second.number == 203)
-    //     tree.printstats(), std::cin.get();
-    if (tree.size() > maxsize)
-    {
-        // printass(*iter);
-        // std::cout << std::endl;
-        // tree.printstats();
-        // tree.printenvs();
-        // tree.navigate();
-    }
-    else if (unexpected(tree.empty(), "empty tree for", iter->first))
-        return 0;
-    else if (unexpected(tree.value() < WDL::WIN, "game value", tree.value()))
-        return tree.navigate(), 0;
-    else if (unexpected(!tree.checkproof(iter), "wrong proof", tree.proof()))
-        return tree.navigate(), 0;
-    else if (iter->first == "biass_")
-        tree.writeproof((std::string(iter->first) + ".txt").c_str());
-    return tree.size();
-}
