ddc88a74ab45aa640873fdc0134ceb4c6a0899fc
fanzheng1729 <58038971+fanzheng1729@users.noreply.github.com>
2026-04-25 10:42:56 +0200

    analyze.cpp

--- src/proof/analyze.cpp
+++ src/proof/analyze.cpp
@@ -129,6 +129,16 @@ static bool hasallvars(RPNspan span1, RPNspan span2)
     return true;
 }
 
+static Hypsize number(RPNstep x)
+{ return x.ishyp() ? 0-x.var().id : x.isthm() ? x.pass->second.number : 0; }
+static bool compstepstable(RPNstep x, RPNstep y)
+{ return number(x) < number(y); }
+static bool compspanstable(RPNspan x, RPNspan y)
+{
+    return std::lexicographical_compare
+    (x.first, x.second, y.first, y.second, compstepstable);
+}
+
 // Map: RPNstep -> is in a tree governed by the step
 typedef std::map<RPNstep, bool, std::less<const char *> > Instep;
 
@@ -150,7 +160,7 @@ static void maxabs
             // Record the spans.
             GovernedRPNspans & spans = result[root];
             if (spans.empty())
-                spans = GovernedRPNspans(compspan);
+                spans = GovernedRPNspans(compspanstable);
             spans[subexp.first] = ast(subexp.first);
         }
         *pinstep = true;
